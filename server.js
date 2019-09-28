2f9dc792fcbdf50a51bba843867d0fa3ffb5e703
Divya Kistapuram <divyani.02@gmail.com>
2019-09-29 00:11:05 +0530

    Docker changes

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
-const app = express();
+const app = express()
+const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
 
-app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
-});
+app.use(express.static('public'))
+app.get('/', (req, res) => res.sendFile(__dirname+'/public/views/index.html'))
+app.get('/generic', (req, res) => res.sendFile(__dirname+'/public/views/generic.html'))
+app.get('/elements', (req, res) => res.sendFile(__dirname+'/public/views/elements.html'))
+
+app.get('/welcome', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
